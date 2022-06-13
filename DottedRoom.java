947a99c820108aa7f8c60cc1f81157f6b352d4af
RyanZareh <106620535+RyanZareh@users.noreply.github.com>
2022-06-13 17:58:10 -0400

    Add files via upload

---
+++ DottedRoom.java
@@ -0,0 +1,32 @@
+/** [DottedRoom.java]
+  * ICS4U1-02
+  * @Richard Yang
+  * @Ryan Zareh
+  * @version 1.0
+  * June 2022
+  */
+
+public class DottedRoom extends Room {
+  
+  public DottedRoom(double x, double y) {
+    super(x,y,9,9);
+  }
+  
+  protected void generateRoomWalls() {
+    for (int i = 0; i < horizontalDimension; i++) {
+      generateWall(i, 0);
+      generateWall(i, verticalDimension - 1);
+    }
+    for (int i = 1; i < verticalDimension - 1; i++) {
+      generateWall(0, i);
+      generateWall(horizontalDimension - 1, i);
+    }
+ generateWall(4, 8);
+ generateWall(2, 3);
+ generateWall(4, 6);
+ generateWall(8, 7);
+ generateWall(3, 7);
+ generateWall(7, 6);
+ generateWall(6, 4);
+  }
+}
---
+++ WalledRoom.java
@@ -0,0 +1,43 @@
+/** [WalledRoom.java]
+  * ICS4U1-02
+  * @Richard Yang
+  * @Ryan Zareh
+  * @version 1.0
+  * June 2022
+  */
+
+public class WalledRoom extends Room {
+  
+  public WalledRoom(double x, double y) {
+    super(x,y,15,8);
+  }
+  
+  protected void generateRoomWalls() {
+    for (int i = 0; i < horizontalDimension; i++) {
+      generateWall(i, 0);
+      generateWall(i, verticalDimension - 1);
+    }
+    for (int i = 1; i < verticalDimension - 1; i++) {
+      generateWall(0, i);
+      generateWall(horizontalDimension - 1, i);
+    }
+ generateWall(0, 2);
+ generateWall(1, 2);
+ generateWall(2, 2);
+ generateWall(3, 2);
+ generateWall(4, 2);
+ generateWall(5, 2);
+ generateWall(6, 2);
+ generateWall(14, 5);
+ generateWall(13, 5);
+ generateWall(12, 5);
+ generateWall(11, 5);
+ generateWall(10, 5);
+ generateWall(8, 0);
+ generateWall(8, 1);
+ generateWall(8, 2);
+ generateWall(8, 4);
+ generateWall(8, 5);
+ generateWall(8, 6);
+  }
+}
